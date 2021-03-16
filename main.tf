8816765eb4f3b7c3538e595676a2e5f2efacb64f
potsn <s@hyonsok.io>
2021-03-16 22:44:56 +0900

    changed.

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ terraform {
 }         
      
 provider "azurerm" {
-    subscription_id = var.TF_VAR_subscription_id
+    subscription_id = var.subscription_id
     client_id       = var.client_id
     client_secret   = var.client_secret
     tenant_id       = var.tenant_id
