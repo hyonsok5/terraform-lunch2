64c382ee9f883bca4b7b77949f6b917b6be9626f
potsn <s@hyonsok.io>
2021-03-16 17:44:24 +0900

    changed src.

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ terraform {
     resource_group_name = "rg-dev-lunch2-aztech-kc"
     storage_account_name  = "stortfstatelunch2"
     container_name        = "cntrtfstatelunch2"
-    key                   = ${{ secrets.strg_cntr_key }}
+    key                   = ${{ secrets.STRG_CNTR_KEY }}
   }  
 
 }         
