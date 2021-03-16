9156dbdcc74a9ff429c25056c6e269d141dd3491
potsn <s@hyonsok.io>
2021-03-16 22:02:28 +0900

    try again to fix bug.

--- main.tf
+++ main.tf
@@ -1,13 +1,25 @@
 
 
 terraform {
-  required_version = ">= 0.12"
+
+  backend "remote" {
+         # The name of your Terraform Cloud organization.
+         organization = "hyonsokio"
+
+         # The name of the Terraform Cloud workspace to store Terraform state files in.
+         workspaces {
+           name = "terraform-lunch2"
+         }
+       }
+
+  // required_version = ">= 0.12"
    
-  backend "azurerm" {     
-    resource_group_name = "rg-dev-lunch2-aztech-kc"
-    storage_account_name  = "stortfstatelunch2"
-    container_name        = "cntrtfstatelunch2"
-  }  
+  // backend "azurerm" {     
+  //   resource_group_name = "rg-dev-lunch2-aztech-kc"
+  //   storage_account_name  = "stortfstatelunch2"
+  //   container_name        = "cntrtfstatelunch2"
+  // }  
+
  
 }         
     
