c3d5509044648aa7c64a25494195b4296cccaeb5
potsn <s@hyonsok.io>
2021-03-16 20:10:43 +0900

    fixed bug.

--- main.tf
+++ main.tf
@@ -1,3 +1,6 @@
+locals {
+  container_key = var.strg_cntr_key
+}
 
 terraform {
   required_version = ">= 0.12"
@@ -6,7 +9,7 @@ terraform {
     resource_group_name = "rg-dev-lunch2-aztech-kc"
     storage_account_name  = "stortfstatelunch2"
     container_name        = "cntrtfstatelunch2"
-    key                   = var.strg_cntr_key 
+    key                   = local.container_key
   }  
 
 }         
