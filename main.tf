cde68ff3365122dd97ef64919ccf2c300b59c561
potsn <s@hyonsok.io>
2021-03-16 20:15:53 +0900

    fix bug again.

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ terraform {
     resource_group_name = "rg-dev-lunch2-aztech-kc"
     storage_account_name  = "stortfstatelunch2"
     container_name        = "cntrtfstatelunch2"
-    key                   = local.container_key
+    #key                   = local.container_key
   }  
 
 }         
