95b38af8c44a10bc736531fcddf5b81309568ffa
min kim <kmc90mc@lgcns.com>
2021-03-17 11:50:55 +0900

    acr-fronetend creation

---
+++ acr-frontend.tf
@@ -0,0 +1,7 @@
+resource "azurerm_container_registry" "acr_dev_aflunch2_aztech_kc" {
+  name                     = "acrdevaflunch2aztechkc"
+  resource_group_name      = data.azurerm_resource_group.rg_dev_lunch2_aztech_kc.name
+  location                 = var.location
+  sku                      = "Basic"
+  admin_enabled            = false
+}
