8f64c71d7b056347cea71dda6228a0b933f3f712
min kim <kmc90mc@lgcns.com>
2021-03-17 16:54:04 +0900

    fixed error

--- loadbalancer.tf
+++ loadbalancer.tf
@@ -1,13 +1,13 @@
 resource "azurerm_public_ip" "pip_lb1_dev_lunch2_aztech_kc" {
   name                = "pip-lb1-dev-lunch2-aztech-kc"
-  location            = var.region
+  location            = var.location
   resource_group_name = data.azurerm_resource_group.rg_dev_lunch2_aztech_kc.name
   allocation_method   = "Static"
 }
   
  resource "azurerm_lb" "lb_dev_lunch2_aztech_kc" {
    name                = "lb-dev-lunch2-aztech-kc"
-   location            = var.region
+   location            = var.location
    resource_group_name = data.azurerm_resource_group.rg_dev_lunch2_aztech_kc.name
  
    frontend_ip_configuration {
