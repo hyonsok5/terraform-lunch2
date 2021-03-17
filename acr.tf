3427555ba9b5371c37fad32968ca44cd3233bfee
김찬양 사운/MS사업팀 <praise.kim@lgcns.com>
2021-03-17 11:41:42 +0900

    acr name fix

--- acr.tf
+++ acr.tf
@@ -1,8 +1,8 @@
 resource "azurerm_container_registry" "acr_dev_ablunch2_aztech_kc" {
-  name                     = "acr-dev-ablunch2-aztech-kc"
+  name                     = "acrdevablunch2aztechkc"
   resource_group_name      = data.azurerm_resource_group.rg_dev_lunch2_aztech_kc.name
   location                 = var.location
-  sku                      = "Premium"
+  sku                      = "Standard"
   admin_enabled            = false
-  georeplication_locations = ["East US", "West Europe"]
+  georeplication_locations = ["Southeast Asia", "East Japan"]
 }
