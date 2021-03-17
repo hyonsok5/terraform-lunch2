027814c8e9cccef4fe43cc28deddc22fdfb62330
potsn <s@hyonsok.io>
2021-03-18 01:44:33 +0900

    added new acr for roulette game

--- acr.tf
+++ acr.tf
@@ -15,8 +15,8 @@ resource "azurerm_container_registry" "acr_dev_aflunch2_aztech_kc" {
   admin_enabled            = true
 }
 
-resource "azurerm_container_registry" "acr_dev_flunch2_aztech_kc" {
-  name                     = "acrdevflunch2aztechkc"
+resource "azurerm_container_registry" "acr_dev_roulette_lunch2_aztech_kc" {
+  name                     = "acrdevroulettelunch2aztechkc"
   resource_group_name      = data.azurerm_resource_group.rg_dev_lunch2_aztech_kc.name
   location                 = var.location
   sku                      = "Standard"
