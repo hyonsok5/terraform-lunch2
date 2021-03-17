0b4294635d951814e0470c793bcab5d21592f816
min kim <kmc90mc@lgcns.com>
2021-03-17 13:04:24 +0900

    modify sku on container registries

--- acr-frontend.tf
+++ acr-frontend.tf
@@ -2,6 +2,6 @@ resource "azurerm_container_registry" "acr_dev_aflunch2_aztech_kc" {
   name                     = "acrdevaflunch2aztechkc"
   resource_group_name      = data.azurerm_resource_group.rg_dev_lunch2_aztech_kc.name
   location                 = var.location
-  sku                      = "Basic"
+  sku                      = "Standard"
   admin_enabled            = false
 }
