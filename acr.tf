5e84915fc01dca1d3d9ecd1251b4e197ca7d3426
potsn <s@hyonsok.io>
2021-03-17 22:11:10 +0900

    acr admin-enabled ok.

--- acr.tf
+++ acr.tf
@@ -3,7 +3,7 @@ resource "azurerm_container_registry" "acr_dev_ablunch2_aztech_kc" {
   resource_group_name      = data.azurerm_resource_group.rg_dev_lunch2_aztech_kc.name
   location                 = var.location
   sku                      = "Standard"
-  admin_enabled            = false
+  admin_enabled            = true
   # georeplication_locations = ["Southeast Asia", "East Japan"]
 }
 
@@ -12,5 +12,5 @@ resource "azurerm_container_registry" "acr_dev_aflunch2_aztech_kc" {
   resource_group_name      = data.azurerm_resource_group.rg_dev_lunch2_aztech_kc.name
   location                 = var.location
   sku                      = "Standard"
-  admin_enabled            = false
+  admin_enabled            = true
 }
