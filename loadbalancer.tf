a1ce1b6743d1d88d54e0384482c4baee7786f946
min kim <kmc90mc@lgcns.com>
2021-03-17 17:11:57 +0900

    update code of lb

--- loadbalancer.tf
+++ loadbalancer.tf
@@ -1,17 +1,43 @@
-resource "azurerm_public_ip" "pip_lb1_dev_lunch2_aztech_kc" {
-  name                = "pip-lb1-dev-lunch2-aztech-kc"
+### LB01
+
+
+resource "azurerm_public_ip" "pip_lb_dev_lunch2_aztech_kc_001" {
+  name                = "pip-lb-dev-lunch2-aztech-kc-001"
   location            = var.location
   resource_group_name = data.azurerm_resource_group.rg_dev_lunch2_aztech_kc.name
   allocation_method   = "Static"
 }
   
- resource "azurerm_lb" "lb_dev_lunch2_aztech_kc" {
-   name                = "lb-dev-lunch2-aztech-kc"
+ resource "azurerm_lb" "lb_dev_lunch2_aztech_kc_001" {
+   name                = "lb-dev-lunch2-aztech-kc-001"
    location            = var.location
    resource_group_name = data.azurerm_resource_group.rg_dev_lunch2_aztech_kc.name
  
    frontend_ip_configuration {
-     name                 = "frntIpLbAks"
-     public_ip_address_id = azurerm_public_ip.pip_lb1_dev_lunch2_aztech_kc.id
+     name                 = "frntIpLbAks001"
+     public_ip_address_id = azurerm_public_ip.pip_lb_dev_lunch2_aztech_kc_001.id
+   } 
+ }
+
+
+
+
+### LB02
+
+resource "azurerm_public_ip" "pip_lb_dev_lunch2_aztech_kc_002" {
+  name                = "pip-lb-dev-lunch2-aztech-kc-002"
+  location            = var.location
+  resource_group_name = data.azurerm_resource_group.rg_dev_lunch2_aztech_kc.name
+  allocation_method   = "Static"
+}
+  
+ resource "azurerm_lb" "lb_dev_lunch2_aztech_kc_002" {
+   name                = "lb-dev-lunch2-aztech-kc-002"
+   location            = var.location
+   resource_group_name = data.azurerm_resource_group.rg_dev_lunch2_aztech_kc.name
+ 
+   frontend_ip_configuration {
+     name                 = "frntIpLbAks002"
+     public_ip_address_id = azurerm_public_ip.pip_lb_dev_lunch2_aztech_kc_002.id
    } 
  }
