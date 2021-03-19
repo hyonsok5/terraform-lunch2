ddbc7ee88e18cc12fafaabb45f5d962d05c68d46
ChahYang Kim <praise.kim@lgcns365.com>
2021-03-19 17:08:05 +0900

    cosmosdb sql db

--- cosmosdb.tf
+++ cosmosdb.tf
@@ -21,3 +21,40 @@ resource "azurerm_cosmosdb_account" "cosmosdb_dev_lunch2_aztech_kc" {
     failover_priority = 0
   }
 }
+
+resource "azurerm_cosmosdb_sql_database" "cosmossql_dev_lunch2_aztech_kc" {
+  name                = "cosmossql-dev-lunch2-aztech-kc"
+  resource_group_name = data.azurerm_resource_group.rg_dev_lunch2_aztech_kc.name
+  account_name        = data.azurerm_cosmosdb_account.cosmosdb_dev_lunch2_aztech_kc.name
+  throughput          = 400
+}
+
+# resource "azurerm_cosmosdb_sql_container" "example" {
+#   name                  = "cosmoscontainer-dev-lunch2-aztech-kc"
+#   resource_group_name   = azurerm_cosmosdb_account.example.resource_group_name
+#   account_name          = azurerm_cosmosdb_account.example.name
+#   database_name         = azurerm_cosmosdb_sql_database.example.name
+#   partition_key_path    = "/definition/id"
+#   partition_key_version = 1
+#   throughput            = 400
+
+#   indexing_policy {
+#     indexing_mode = "Consistent"
+
+#     included_path {
+#       path = "/*"
+#     }
+
+#     included_path {
+#       path = "/included/?"
+#     }
+
+#     excluded_path {
+#       path = "/excluded/?"
+#     }
+#   }
+
+#   unique_key {
+#     paths = ["/definition/idlong", "/definition/idshort"]
+#   }
+# }
