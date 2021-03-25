5d82afc7831a3387dd29ec0942fb0a30d10dabf5
min kim <kmc90mc@lgcns.com>
2021-03-25 17:28:38 +0900

    error fix

--- vm.tf
+++ vm.tf
@@ -16,8 +16,8 @@ resource "azurerm_public_ip" "pip_dev_eslunch2_aztech_kc" {
   name                = "pip-dev-eslunch2-aztech-kc"
   resource_group_name      = data.azurerm_resource_group.rg_dev_lunch2_aztech_kc.name
   location                 = var.location
-  allocation_method   = "Dynamic"
-
+  allocation_method   = "Static"
+  sku = "Standard"
 }  
 
 resource "azurerm_network_interface" "nic_es_dev_lunch2_aztech_kc" {
@@ -39,25 +39,25 @@ resource "azurerm_network_interface" "nic_es_dev_lunch2_aztech_kc" {
 #   name                = "vm-es-dev-lunch2-aztech-kc"
 #   resource_group_name = data.azurerm_resource_group.rg_dev_lunch2_aztech_kc.name
 #   location            = var.location
-#   size                = "Standard_F2"
-#   # admin_username      = "adminuser"
+#   size                = "Standard_B2s"
+#   admin_username      = "adminuser"
 #   network_interface_ids = [
 #     azurerm_network_interface.nic_es_dev_lunch2_aztech_kc.id,
 #   ]
 
-#   # admin_ssh_key {
-#   #   username   = "adminuser"
-#   #   public_key = file("~/.ssh/id_rsa.pub")
-#   # }
+#   admin_ssh_key {
+#     username   = "adminuser"
+#     public_key = file("~/.ssh/id_rsa.pub")
+#   }
 
-#   os_profile {
-#     computer_name  = "vm-es-dev-lunch2-aztech-kc"
-#     admin_username = "testadmin"
-#     admin_password = "Password1234!"
-#   }
-#   os_profile_linux_config {
-#     disable_password_authentication = false
-#   }
+#   # os_profile {
+#   #   computer_name  = "vm-es-dev-lunch2-aztech-kc"
+#   #   admin_username = "testadmin"
+#   #   admin_password = "Password1234!"
+#   # }
+#   # os_profile_linux_config {
+#   #   disable_password_authentication = false
+#   # }
 
 #   os_disk {
 #     caching              = "ReadWrite"
@@ -71,3 +71,41 @@ resource "azurerm_network_interface" "nic_es_dev_lunch2_aztech_kc" {
 #     version   = "latest"
 #   }
 # }
+
+
+
+resource "azurerm_virtual_machine" "vm_es_dev_lunch2_aztech_kc" {
+  name                  = "vm-es-dev-lunch2-aztech-kc"
+  location              = var.location
+  resource_group_name   = data.azurerm_resource_group.rg_dev_lunch2_aztech_kc.name
+  network_interface_ids = [azurerm_network_interface.nic_es_dev_lunch2_aztech_kc.id]
+  vm_size               = "Standard_B2s"
+
+  # Uncomment this line to delete the OS disk automatically when deleting the VM
+  # delete_os_disk_on_termination = true
+
+  # Uncomment this line to delete the data disks automatically when deleting the VM
+  # delete_data_disks_on_termination = true
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+  storage_os_disk {
+    name              = "myosdisk1"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = "vm-es-dev-lunch2-aztech-kc"
+    admin_username = "testadmin"
+    admin_password = "Password1234!"
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+
+}
